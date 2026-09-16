6674c8b16679777a8bda3c801d570779cfb3850d
FarhanTips <farhanakbor02@gmail.com>
2026-09-16 18:18:15 +0600

    first next js file

---
+++ 1. Next JS Foundation and Routing/my-nex-js-app/src/app/docs/page.jsx
@@ -0,0 +1,11 @@
+
+
+const DocsPage = () => {
+    return (
+        <div>
+            <h2>Our Official Documentation</h2>
+        </div>
+    );
+};
+
+export default DocsPage;
---
+++ 1. Next JS Foundation and Routing/my-nex-js-app/src/app/showcase/page.jsx
@@ -0,0 +1,13 @@
+
+
+import React from 'react';
+
+const ShowcasePage = () => {
+    return (
+        <div>
+            <h2>Our Showcases</h2>
+        </div>
+    );
+};
+
+export default ShowcasePage;
