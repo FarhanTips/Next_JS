8f68dc7029c069a1bf71fb3299f04ef8f328d539
FarhanTips <farhanakbor02@gmail.com>
2026-09-16 18:53:09 +0600

    Page, Route and Nested Route

---
+++ 1. Next JS Foundation and Routing/my-nex-js-app/src/app/about/developers/page.jsx
@@ -0,0 +1,13 @@
+
+
+import React from 'react';
+
+const DevelopersPage = () => {
+    return (
+        <div>
+            <h2>Info of developers</h2>
+        </div>
+    );
+};
+
+export default DevelopersPage;
