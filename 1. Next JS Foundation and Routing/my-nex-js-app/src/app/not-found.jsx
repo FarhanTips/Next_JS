fba07162fafebe163aa956f080055de651a7ab54
FarhanTips <farhanakbor02@gmail.com>
2026-09-18 03:10:50 +0600

    Global Not Found

---
+++ 1. Next JS Foundation and Routing/my-nex-js-app/src/app/not-found.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFoundGlobal = () => {
+    return (
+        <div>
+            <h2>Our Custom Not Found</h2>
+        </div>
+    );
+};
+
+export default NotFoundGlobal;
