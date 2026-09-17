bbc2d4facbf7b74125a9155c53a17cdcf892e9ac
FarhanTips <farhanakbor02@gmail.com>
2026-09-18 03:32:04 +0600

    About Not Found

---
+++ 1. Next JS Foundation and Routing/my-nex-js-app/src/app/about/not-found.jsx
@@ -0,0 +1,11 @@
+
+import React from 'react';
+const NotFoundAbout = () => {
+    return (
+        <div>
+            <h1>Your input does not exist in About</h1>
+        </div>
+    );
+};
+
+export default NotFoundAbout;
