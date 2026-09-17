c238921197cdff11d47eeb13b77a148f8cf6436a
FarhanTips <farhanakbor02@gmail.com>
2026-09-17 21:04:51 +0600

    Image optimization using Next JS <Image>

--- 1. Next JS Foundation and Routing/my-nex-js-app/next.config.mjs
+++ 1. Next JS Foundation and Routing/my-nex-js-app/next.config.mjs
@@ -2,6 +2,18 @@
 const nextConfig = {
   /* config options here */
   reactCompiler: true,
+  images: {
+    remotePatterns: [
+      // https://images.unsplash.com/flagged/photo-1550413231-202a9d53a331
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        port: '',
+        pathname: '/dfwshshcn/**',
+        search: '',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
--- 1. Next JS Foundation and Routing/my-nex-js-app/src/app/about/page.jsx
+++ 1. Next JS Foundation and Routing/my-nex-js-app/src/app/about/page.jsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import Image from 'next/image';
+
 
 const AboutPage = () => {
     return (
         <div>
             <h2>About Us</h2>
+            {/* <img
+                className="w-[500px] h-[300px]"
+                src="https://images.unsplash.com/flagged/photo-1550413231-202a9d53a331"
+                alt="About us"
+            /> */}
+
+            <Image src={"/Images/Convo Pic.jpeg"} width={300} height={500} alt='Profilre Picture'></Image>
+
+            <Image src={"https://res.cloudinary.com/dfwshshcn/image/upload/t_Banner%2016:9/v1721825444/College_Football_Classic_pmnxip.jpg"} alt='Football Field' width={500} height={300}></Image>
+
+
         </div>
     );
 };
