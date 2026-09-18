933e0cd49c0ac7299f9c7152e41e0ef381dd83fe
FarhanTips <farhanakbor02@gmail.com>
2026-09-19 01:55:46 +0600

    Loading added in menu

---
+++ New folder Next JS Rendering Basics/my-nextjs-second-app/src/app/menu/loading.jsx
@@ -0,0 +1,10 @@
+
+const MenuLoading = () => {
+    return (
+        <div className="min-h-screen flex justify-center items-center">
+            <span className="loading loading-spinner loading-xl"></span>
+        </div>
+    );
+};
+
+export default MenuLoading;
