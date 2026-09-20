a6b34f81c833015f0054d427edf0e330b8d0e9c3
FarhanTips <farhanakbor02@gmail.com>
2026-09-20 20:03:38 +0600

    Fetching data in Server Component Posts

--- 3. Advanced Data Fetching and UI Integration/nextjs-data-cacshing/src/app/posts/page.jsx
+++ 3. Advanced Data Fetching and UI Integration/nextjs-data-cacshing/src/app/posts/page.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 
-const PostsPage = () => {
+const getPosts = async () => {
+    const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+    return res.json();
+
+}
+
+const PostsPage = async () => {
+    const posts = await getPosts();
     return (
         <div>
-            <h2>Hello Posts</h2>
+            <h2>Hello Posts: {posts.length}</h2>
         </div>
     );
 };
