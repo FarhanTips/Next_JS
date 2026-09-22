f2bd84b681bcc9b7f1c959de0c28588f69bde7f9
FarhanTips <farhanakbor02@gmail.com>
2026-09-22 17:55:34 +0600

    BooksContext Added

--- 4. Next jS Project/book-vibe-next-js/src/app/layout.tsx
+++ 4. Next jS Project/book-vibe-next-js/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Nav from "@/Components/Nav";
+import BooksProvider from "@/context/BooksContext";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,9 +26,11 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
-        <Nav></Nav>
-        {children}
-        </body>
+        <BooksProvider>
+          <Nav></Nav>
+          {children}
+        </BooksProvider>
+      </body>
     </html>
   );
 }
